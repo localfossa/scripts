e13c41047e70b590b02bf3e63ca13a15989429bc
localfossa <74490107+localfossa@users.noreply.github.com>
2022-05-03 18:05:06 +0300

    Changed wait to task.wait and renamed services

--- universal-auto-clicker/main.lua
+++ universal-auto-clicker/main.lua
@@ -1,8 +1,8 @@
 --Already Running--
 if getgenv()["Already Running"] then return else getgenv()["Already Running"] = "" end
 --Services--
-local UIS = game:GetService("UserInputService")
-local VIM = game:GetService("VirtualInputManager")
+local UserInputService = game:GetService("UserInputService")
+local VirtualInputManager = game:GetService("VirtualInputManager")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 --Vars--
@@ -12,7 +12,7 @@ local flags = {Auto_Clicking = false, Mouse_Locked = false}
 --Mouse-- 
 local Mouse = setmetatable({}, {
     __index = function(table, index)
-        return UIS:GetMouseLocation()[index]
+        return UserInputService:GetMouseLocation()[index]
     end
 })
 --Get Keybind--
@@ -39,7 +39,7 @@ local Text = Draw("Text", {
     Visible = true,
 })
 --Key Bind--
-UIS.InputBegan:Connect(function(inputObj, GPE)
+UserInputService.InputBegan:Connect(function(inputObj, GPE)
     if (not GPE) then
         if (inputObj.KeyCode == getKeycode(Settings["Auto Click Keybind"])) then
             flags.Auto_Clicking = not flags.Auto_Clicking
@@ -62,9 +62,9 @@ coroutine.wrap(function()
         if (flags.Auto_Clicking) then
             for i = 1, 2 do
                 if (flags.Mouse_Locked) then
-                    VIM:SendMouseButtonEvent(flags.Mouse_Locked_Position.X, flags.Mouse_Locked_Position.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                    VirtualInputManager:SendMouseButtonEvent(flags.Mouse_Locked_Position.X, flags.Mouse_Locked_Position.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
                 else
-                    VIM:SendMouseButtonEvent(Mouse.X, Mouse.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                    Virtual:SendMouseButtonEvent(Mouse.X, Mouse.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
                 end
             end
         end
@@ -72,7 +72,7 @@ coroutine.wrap(function()
         if (Settings.Delay <= 0) then
             RunService.RenderStepped:Wait()
         else
-            wait(Settings.Delay)
+            task.wait(Settings.Delay)
         end
     end
 end)()
